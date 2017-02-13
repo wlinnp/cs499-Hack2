11967a6715d90cb50744b0d185882333e0b38070
wlphyo <wlphyo@cpp.edu>
2017-02-13 13:53:07 -0800

    fundamental settings + init adding entries

--- handler.js
+++ handler.js
@@ -1,13 +1,33 @@
+
 'use strict';
+var request = require('request');
+
+var response = {
+    statusCode: 200,
+    headers: {
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+    },
+    body: "success"
+};
+var url = "https://rqato4w151.execute-api.us-west-1.amazonaws.com/dev/info";
+var tableName = "CS499Hack2BroncoExpress";
+
+module.exports.PullData = (event, context, callback) => {
+    request(url, function (error, response, body) {
+        if (error) {
+            console.error(error);
+            res.send("getting XML Error");
+        } else {
+            var allObjects = JSON.parse(body);
+            for (var i = 0; i < allObjects.length; i++) {
+                console.log(allObjects[i]);
+            }
+        }
+    });
+};
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+
 
   callback(null, response);
 
